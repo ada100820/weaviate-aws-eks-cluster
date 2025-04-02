27cc4183db3a9f0cccb39c835e658813d77c0ace
Adarsh Shukla <89727799+ada100820@users.noreply.github.com>
2025-04-02 04:17:35 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,27 +3,37 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
 variable "cluster_name" {
   type    = string
   default = "weaviate-eks"
 }
 
-variable "vpc_id" {
-  type = string
-  # e.g., "vpc-12345678"
-}
-
-variable "private_subnets" {
-  type = list(string)
-  # e.g., ["subnet-aaaaaaa","subnet-bbbbbbb"]
-}
-
-variable "public_subnets" {
-  type = list(string)
-  # e.g., ["subnet-ccccccc","subnet-ddddddd"]
+variable "cluster_version" {
+  type    = string
+  default = "1.25"
 }
 
 variable "desired_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "max_capacity" {
   type    = number
   default = 3
 }
