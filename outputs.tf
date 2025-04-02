d326c97424ff7653ef0245b8aae562174eb145b5
Adarsh Shukla <89727799+ada100820@users.noreply.github.com>
2025-04-02 04:23:05 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,24 @@
-output "cluster_name" {
+output "vpc_id" {
+  description = "ID of the newly created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "eks_cluster_name" {
   description = "Name of the EKS cluster"
   value       = module.eks.cluster_id
 }
 
-output "cluster_endpoint" {
+output "eks_cluster_endpoint" {
   description = "Endpoint of the EKS cluster"
   value       = module.eks.cluster_endpoint
 }
